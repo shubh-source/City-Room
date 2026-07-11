a3feca75c2a83990e77e869064e4a2cc0a780184
shubh-source <shubhkatiyar6513@gmail.com>
2026-07-11 16:31:14 +0530

    fix: load firebase key from env for render deploy

--- backend/firebaseAdmin.js
+++ backend/firebaseAdmin.js
@@ -1,6 +1,11 @@
 const admin = require('firebase-admin');
 
-const serviceAccount = require('./serviceAccountKey.json');
+let serviceAccount;
+if (process.env.FIREBASE_B64) {
+  serviceAccount = JSON.parse(Buffer.from(process.env.FIREBASE_B64, 'base64').toString('utf8'));
+} else {
+  serviceAccount = require('./serviceAccountKey.json');
+}
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
